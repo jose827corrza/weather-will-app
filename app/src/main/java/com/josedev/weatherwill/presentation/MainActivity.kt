a9a848c02dcc806d2d6bd0ee3b6e2bf89c53cd77
Jose Corredor <jose96corrza@gmail.com>
2023-10-06 01:05:28 -0500

    Views completed,first stage of the project complete

--- app/src/main/java/com/josedev/weatherwill/presentation/MainActivity.kt
+++ app/src/main/java/com/josedev/weatherwill/presentation/MainActivity.kt
@@ -5,18 +5,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.result.ActivityResultLauncher
-import androidx.activity.result.contract.ActivityResultContract
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.josedev.weatherwill.presentation.ui.theme.DarkBlue
 import com.josedev.weatherwill.presentation.ui.theme.DeepBlue
 import com.josedev.weatherwill.presentation.ui.theme.WeatherWillTheme
@@ -53,14 +59,35 @@ class MainActivity : ComponentActivity() {
 //                ) {
 //
 //                }
-                Column (
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .background(DarkBlue)
+                Box (
+                    modifier = Modifier.fillMaxSize()
                 ){
-                    WeatherCard(state = viewModel.state, backgroundColor = DeepBlue)
-                    Text("test")
+                    Column (
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .background(DarkBlue)
+                    ){
+                        WeatherCard(state = viewModel.state, backgroundColor = DeepBlue)
+
+                        Spacer(modifier = Modifier.height(16.dp))
+
+                        WeatherForecast(state = viewModel.state)
+                    }
+                    if (viewModel.state.isLoading){
+                        CircularProgressIndicator(
+                            modifier = Modifier.align(Alignment.Center)
+                        )
+                    }
+                    viewModel.state.error?.let { error ->
+                        Text(
+                            text = error,
+                            color = Color.Red,
+                            textAlign = TextAlign.Center,
+                            modifier = Modifier.align(Alignment.Center)
+                        )
+                    }
                 }
+
             }
         }
     }
